0352566160384e6779b2d48e7fc5f5e76a056f5d
lucasmancan <lucasfmancan@hotmail.com>
2021-09-20 21:15:06 -0300

    Adicionando estrutura inicial

---
+++ src/test/kotlin/br/com/lucasmancan/dojojpa/DojoJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.lucasmancan.dojojpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DojoJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
